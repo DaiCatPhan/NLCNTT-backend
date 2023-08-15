45dabbe3e3b92981097f50834d6f89b03db55cd4
Dai Cat <phandaicat12032002@gmail.com>
2023-08-15 21:26:27 +0700

    logic register and login

--- src/controllers/AuthenticationController.js
+++ src/controllers/AuthenticationController.js
@@ -1,37 +1,81 @@
 import Account from "../modules/Account";
-import bcrypt from "bcrypt";
+import bcrypt, { hash } from "bcrypt";
+import jwt from "jsonwebtoken";
+
+var privateKey = "sadfdsdf";
 
 class Authentication {
   // [POST] /authentication/register
-  register(req, res, next) {
-    // const { email , username , password , mssv ,  classs  } = req.body;
-    const { email, password, ...rest } = req.body;
+  async register(req, res, next) {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) {
+      // 400 Lỗi chính nó xảy ra lun
+      return res.status(400).json({ message: "Chua truyen du tham so" });
+    }
 
-    bcrypt
-      .hash(password, 10)
-      .then((hash) => {
-        // Nếu như băm thành công thì trả về mật khẩu đã được bâm ở biến hash
-        Account.findOne({ email: email })
-          .then((user) => {
-            if (user) {
-              res.json("Tài khoản đã tồn tại !!!");
-            } else {
-              Account.create({ email, password: hash, ...rest })
-                .then((user) => {
-                  console.log("create success : ", user);
-                  res.json("Success");
-                })
-                .catch((err) => res.json("Create an account fail !!! ")); 
-            }
-          })
-          .catch((err) => res.json(err));
-      })
-      .catch((err) => console.log(err));
+    try {
+      const userExited = await Account.findOne({ email: email });
+      if (userExited) {
+        return res.status(200).json("Tài khoản đã tồn tại !!!");
+      }
+      // Hash
+      const hash = await bcrypt.hash(password, 10);
+      if (hash) {
+        const userDoc = await Account.create({
+          email,
+          password: hash,
+          username,
+        });
+        if (userDoc) {
+          delete userDoc.password;
+          return res.status(200).json({ message: "Success", data: userDoc });
+        } else {
+          return res.status(401).json("Tao tai khoan that bai !");
+        }
+      } else {
+        return res.status(402).json("Hash pass fail !!");
+      }
+    } catch (e) {
+      return res.status(500).json(" Loi server ");
+    }
   }
 
   // [POST] /authentication/login
-  login(req, res, next) {
-    res.json("Login");
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json("Du lieu ch day du !!!");
+    }
+
+    try {
+      const user = await Account.findOne({ email: email });
+      if (!user) {
+        return res.status(401).json("Nguoi dung k ton tai");
+      }
+      const passwordHash = user.password;
+      const passInvalid = await bcrypt.compare(password, passwordHash); // true false
+      if (passInvalid) {
+        // Luu cookie
+        const token = jwt.sign({ email: user.email }, privateKey); // chuoi json duoc ma hoa
+        if (token) {
+          res.cookie("token", token, {
+            // lưu lên cookie
+            sameSite: true,
+            secure: false,
+            httpOnly: true,
+          });
+        }
+
+        return res.status(200).json({ message: "Success", user, token });
+      } else {
+        return res.status(400).json({ message: "Mat khau khong dung" });
+      }
+    } catch (e) {
+      return res.status(500).json("Loi server !");
+    }
+
+    // const hashPass = await bcrypt.compare(password, 'sadfsa');
+    // return res.status(200).json(hashPass);
   }
 }
 
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import AuthenticationController from "../controllers/AuthenticationController";
 
-router.post("/register", AuthenticationController.register);
-router.post("/login", AuthenticationController.login);
+router.post("/register", AuthenticationController.register); 
+router.post("/login", AuthenticationController.login); 
 
 export default router;
